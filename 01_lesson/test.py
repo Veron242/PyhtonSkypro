dddfdc461c7fa4eec516ea3403e1f4ff942d661c
Veron242 <konstantinova9067@gmail.com>
2025-06-30 20:12:35 +0300

    lesson2

---
+++ 01_lesson/test.py
@@ -0,0 +1,26 @@
+# print("hello world test")
+
+#my_heigh = 160
+#print(my_heigh)
+
+#my_name = "Veronika"
+#my_name = "Veronika Konstantinova"
+#print(my_name)
+
+#pet_name = input("Как зовут вашего питомца?")
+#print("Ваш любимчик - " + pet_name)
+
+#def print_python():
+#    print("Учу Python!")
+#print_python()
+
+def print_letter(let):
+    print(let, end='')
+
+print_letter('С')
+print_letter('т')
+print_letter('у')
+print_letter('д')
+print_letter('е')
+print_letter('н')
+print_letter('т')
