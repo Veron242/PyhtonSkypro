b7e358b1c1d901b11ecd24cc1050c1d2581f324e
Veron242 <konstantinova9067@gmail.com>
2025-07-20 12:34:12 +0300

    6 урок

---
+++ 06_lesson/lesson06_task2.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+driver.get("http://uitestingplayground.com/textinput")
+
+placeholder_field = driver.find_element(By.ID, "newButtonName")
+placeholder_field.send_keys("SkyPro")
+
+blue_button = driver.find_element(By.CSS_SELECTOR, "button.btn")
+blue_button.click()
+
+print(blue_button.text)
+
+driver.quit()
